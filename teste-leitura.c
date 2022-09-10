3b89be48455840c6c7d295a393aa1c25aa2dfca6
dudaac1 <dudaac99@gmail.com>
2022-09-10 19:15:08 -0300

    mostrando matriz

--- teste-leitura.c
+++ teste-leitura.c
@@ -5,20 +5,33 @@
 typedef struct tsp_data {
   char ** files;
   int count;
-} FILES;
+} Files;
 
-FILES * ListDirectoryContents(); //recupera o nome dos arquivos das matrizes de custo
-void ChooseTSPFile(FILES * data);
+Files * ListDirectoryContents(); //recupera o nome dos arquivos das matrizes de custo
+void ChooseTSPFile(Files * data);
 
 int main() {
   printf("\nTRAVELLING SALESMANN PROBLEM - BEM-VINDO\n");
-  printf("Listando arquivos das Matrizes de Custo:\n");
-  FILES * tsp_data = ListDirectoryContents();
-  ChooseTSPFile(tsp_data);
+  // printf("Listando arquivos das Matrizes de Custo:\n");
+  // Files * tsp_data = ListDirectoryContents();
+  // ChooseTSPFile(tsp_data);
+
+  //usando menor matriz de custo
+  FILE * arquivo;
+  char nomeArquivo[30] = "tsp1_253.txt";
+  if ((arquivo = fopen(nomeArquivo, "r")) == NULL) {
+    printf("erro");
+    exit(1);
+  }
+  char texto[100];
+  while(fgets(texto, 100, arquivo) != NULL) //imprime matriz
+    printf("%s", texto);
+
+  
 }
 
-FILES * ListDirectoryContents() {
-  FILES * tsp_data = (FILES*)malloc(sizeof(FILES));
+Files * ListDirectoryContents() {
+  Files * tsp_data = (Files*)malloc(sizeof(Files));
   tsp_data->files = NULL;
   tsp_data->count = 0;
 
@@ -40,6 +53,7 @@ FILES * ListDirectoryContents() {
 
   do {
     ++tsp_data->count;
+    //ta dando erro aqui, eu acho
     (tsp_data->files) = (char**)realloc(tsp_data->files, sizeof(char) * tsp_data->count);
     (tsp_data->files)[tsp_data->count] = (char*)malloc(sizeof(char)*strlen(file.cFileName));
     printf("\tArquivo [%d]: %s\n", tsp_data->count, file.cFileName);
@@ -49,7 +63,7 @@ FILES * ListDirectoryContents() {
   return tsp_data;
 }
 
-void ChooseTSPFile(FILES * data) {
+void ChooseTSPFile(Files * data) {
   int number;
   do {
     printf("Escolha um dos arquivos para executar o codigo: ");
